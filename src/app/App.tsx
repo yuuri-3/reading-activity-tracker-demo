746fb82049738919e2e3aff60f90997359d1281e
yuuri-3 <yr.lunlun@gmail.com>
2026-01-01 20:34:34 +0900

    Fix layout height and inner scrolling

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -52,10 +52,10 @@ function AppContent() {
   }, []);
 
   return (
-    <div className="size-full flex flex-col">
+    <div className="relative h-dvh w-full min-h-0 overflow-hidden flex flex-col">
       {/* Main Content */}
-      <main className="flex-1 overflow-hidden">
-        <div className="h-full w-full">
+      <main className="flex-1 min-h-0 overflow-hidden">
+        <div className="h-full w-full min-h-0">
           {currentPage === "home" && <TimerPage />}
           {currentPage === "books" && <BookCollectionView />}
           {currentPage === "records" && <RecordSingleView />}
@@ -64,7 +64,7 @@ function AppContent() {
       </main>
 
       {/* Bottom Navigation - iOS Floating Style */}
-      <nav className="fixed bottom-0 left-0 right-0 pointer-events-none">
+      <nav className="absolute bottom-0 left-0 right-0 pointer-events-none">
         <div className="max-w-2xl mx-auto px-4 pb-4">
           <div className="pointer-events-auto -translate-y-6 max-w-[345px] mx-auto">
             <TabBar currentPage={currentPage} onChange={setCurrentPage} />
